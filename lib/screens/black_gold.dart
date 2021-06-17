1b37c9bfb6aa4ed43103845bcd8cbdfe2a8aba57
shubhamp269 <patilshubham269@gmail.com>
2021-06-17 12:52:33 +0530

    first commit

---
+++ lib/screens/black_gold.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class Blackgold extends StatefulWidget {
+  @override
+  _BlackgoldState createState() => _BlackgoldState();
+}
+
+class _BlackgoldState extends State<Blackgold> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Black gold'),
+    );
+  }
+}
